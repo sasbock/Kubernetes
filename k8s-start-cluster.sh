bbbb6f89cfe1cd8d86c4a340225e58eed809d481
Stefan Asbock <sasbock@gmail.com>
2026-06-22 07:25:45 -0700

    _

--- k8s-start-cluster.sh
+++ k8s-start-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
+#export KUBECONFIG=/etc/kubernetes/admin.conf
 kubeadm init --pod-network-cidr=10.244.0.0/16
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
