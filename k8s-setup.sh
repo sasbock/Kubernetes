e38bde9504c905da35b93b4ee5dbc7bab9f6c03f
Stefan Asbock <39928688+sasbock@users.noreply.github.com>
2026-07-02 15:55:02 -0700

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -92,7 +92,13 @@ fedora)
 	;;
 ubuntu)
 	apt update
-	apt install -y net-tools kubelet kubeadm kubectl kubernetes-cni containerd
+	apt install -y net-tools ca-certificates curl kubelet kubeadm kubectl kubernetes-cni 
+	install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	chmod a+r /etc/apt/keyrings/docker.asc
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	systemctl enable --now docker
 	;;
 esac
 
