cc480e133cf21343708ff1e5f359dabd3ac345cf
Stefan Asbock <sasbock@gmail.com>
2026-07-06 12:36:16 -0700

    _

--- k8s-create-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubeadm init \
-  --apiserver-advertise-address=192.168.56.2 \
-  --control-plane-endpoint=192.168.56.2:6443 \
-  --pod-network-cidr=10.244.0.0/16
-
-# curl -sSLO https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
-# kubectl apply -f kube-flannel.yml
